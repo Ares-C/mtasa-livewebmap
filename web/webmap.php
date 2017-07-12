9772a4b7b3e84b0ea40fd86bcda8ffa931810fe7
Ares (TC) <aresmta@gmail.com>
2017-07-12 17:40:02 -0300

    Add files via upload

---
+++ web/webmap.php
@@ -0,0 +1,31 @@
+<?php
+
+/*
+----------------------------------------->>
+-- IGC: International Gaming Community
+-- Date: 06 Feb 2017
+-- Project: Online Live Map
+-- Type: Server Side
+-- Author: Ares
+----------------------------------------->>
+*/
+
+include( "mtaphpsdk_0.4/mta_sdk.php" );
+$mtaServer = new mta("localhost", 22005, "username", "password");
+$resource = $mtaServer->getResource ( "IGCwebmap" );
+
+
+if ($_GET) {
+	$count = $_GET["count"];
+	if ($count) {
+		if ( (int)$count ) {
+			if ( (int)$count >= 150 ) { die("Connection banned."); }
+			$retn = $resource->call("returnServerData", (int)$count);
+			$return = $retn[0];
+
+			$jsonstring = json_encode($return);
+			echo $jsonstring;	
+		}
+	}
+}
+?>
